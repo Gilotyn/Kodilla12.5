0b462d4607ca4e3eadaa17913d00492ed038c8e6
RobertGraupner <robert.graupnerr@gmail.com>
2017-10-07 23:14:27 +0200

    fourth commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,34 @@
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
-function getQuote() {
-	$.getJSON(quoteUrl, createTweet);
-}
-
-function createTweet(input) {
-	if (input.quoteAuthor.length) {
-		input.quoteAuthor = 'Unknown author';
-	}
-
-	var tweetText = 'Quote of the day: ' + input.quoteText + ' Author: ' + input.quoteAuthor;
-
-	if (tweetText.length > 140) {
-		getQuote();
-	} else {
-		var tweet = tweetLink + encodeURIComponent(tweetText);
-		$('.quote').text(input.quoteText);
-		$('.author').text("Author: " + input.quoteAuthor);
-		$('.tweet').attr('href', tweet);
-	}
-}
-
 $(document).ready(function() {
-	getQuote();
+   
+	var prefix = "https://cors-anywhere.herokuapp.com/";
+	var tweetLink = "https://twitter.com/intent/tweet?text=";
+	var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+	function getQuote() {
+	    $.getJSON(prefix + quoteUrl, createTweet);
+	    $.ajaxSetup({ 
+	    	cache: false
+	    });
+	}
+	function createTweet(input) {
+		var data = input[0];
+	    var quoteText = $(data.content).text().trim();
+	    var quoteAuthor = data.title;
+
+	    if (!quoteAuthor.length) {
+	        quoteAuthor = "Unknown author";
+	    }
+	    var tweetText = "Quote of the day: " + quoteText + " Author: " + quoteAuthor;
+	    if (tweetText.length > 140) {
+	    	getQuote();
+		} else {
+	    	var tweet = tweetLink + encodeURIComponent(tweetText);
+		    $('.quote').text(quoteText);
+		    $('.author').text("Author: " + quoteAuthor);
+		    $('.tweet').attr('href', tweet);
+		}
+	}
+	 getQuote();
 	$('.trigger').click(function() {
-		getQuote();
-	})
+        getQuote();
+  	});
 });
